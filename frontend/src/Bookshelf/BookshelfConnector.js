59076b7c801c26f54608a5f706e3f839899e6bbc
Faustvii <Faustvii@users.noreply.github.com>
2025-07-08 21:31:26 +0200

    feat: infinite scroll for books

--- frontend/src/Bookshelf/BookshelfConnector.js
+++ frontend/src/Bookshelf/BookshelfConnector.js
@@ -2,22 +2,26 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
+import { fetchBooksByAuthor } from 'Store/Actions/bookActions';
 import { saveBookshelf, setBookshelfFilter, setBookshelfSort } from 'Store/Actions/bookshelfActions';
 import createAuthorClientSideCollectionItemsSelector from 'Store/Selectors/createAuthorClientSideCollectionItemsSelector';
 import createDimensionsSelector from 'Store/Selectors/createDimensionsSelector';
+import { registerPagePopulator, unregisterPagePopulator } from 'Utilities/pagePopulator';
 import Bookshelf from './Bookshelf';
 
 function createBookFetchStateSelector() {
   return createSelector(
-    (state) => state.books.items.length,
-    (state) => state.books.isFetching,
-    (state) => state.books.isPopulated,
-    (length, isFetching, isPopulated) => {
-      const bookCount = (!isFetching && isPopulated) ? length : 0;
+    (state) => state.books,
+    (booksState) => {
+      const bookCount = (!booksState.isFetching && booksState.isPopulated) ? booksState.items.length : 0;
       return {
         bookCount,
-        isFetching,
-        isPopulated
+        isFetching: booksState.isFetching,
+        isPopulated: booksState.isPopulated,
+        page: booksState.page,
+        totalPages: booksState.totalPages,
+        totalRecords: booksState.totalRecords,
+        pageSize: booksState.pageSize
       };
     }
   );
@@ -36,6 +40,7 @@ function createMapStateToProps() {
         isPopulated,
         isFetching,
         bookCount: books.bookCount,
+        isBooksPopulated: books.isPopulated,
         isSmallScreen: dimensionsState.isSmallScreen
       };
     }
@@ -45,10 +50,57 @@ function createMapStateToProps() {
 const mapDispatchToProps = {
   setBookshelfSort,
   setBookshelfFilter,
-  saveBookshelf
+  saveBookshelf,
+  fetchBooksByAuthor
 };
 
 class BookshelfConnector extends Component {
+
+  //
+  // Lifecycle
+
+  componentDidMount() {
+    registerPagePopulator(this.populate);
+    this.populate();
+  }
+
+  componentDidUpdate(prevProps) {
+    const prevAuthorIds = prevProps.items ? prevProps.items.map((a) => a.id).sort().join(',') : '';
+    const currentAuthorIds = this.props.items ? this.props.items.map((a) => a.id).sort().join(',') : '';
+
+    if (prevAuthorIds !== currentAuthorIds && this.props.items) {
+      this.props.items.forEach((author) => {
+        const statistics = author.statistics;
+        const hasCompleteBookList = statistics && statistics.totalBookCount !== 0 && author.books.length >= statistics.totalBookCount;
+
+        if (!hasCompleteBookList) {
+          this.props.fetchBooksByAuthor({ authorId: author.id });
+        }
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    unregisterPagePopulator(this.populate);
+  }
+
+  //
+  // Control
+
+  populate = () => {
+    const { items } = this.props;
+
+    if (items && items.length > 0) {
+      items.forEach((author) => {
+        const statistics = author.statistics;
+        const hasCompleteBookList = statistics && statistics.totalBookCount !== 0 && author.books.length >= statistics.totalBookCount;
+
+        if (!hasCompleteBookList) {
+          this.props.fetchBooksByAuthor({ authorId: author.id });
+        }
+      });
+    }
+  };
 
   //
   // Listeners
@@ -81,9 +133,12 @@ class BookshelfConnector extends Component {
 }
 
 BookshelfConnector.propTypes = {
+  isSmallScreen: PropTypes.bool.isRequired,
   setBookshelfSort: PropTypes.func.isRequired,
   setBookshelfFilter: PropTypes.func.isRequired,
-  saveBookshelf: PropTypes.func.isRequired
+  saveBookshelf: PropTypes.func.isRequired,
+  fetchBooksByAuthor: PropTypes.func.isRequired,
+  items: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default connect(createMapStateToProps, mapDispatchToProps)(BookshelfConnector);
